e70c5a61f0488ad800bb3b9e7a748ec0fb95a811
Cédric Le Moigne <cedlemo@gmx.com>
2018-08-28 22:20:17 +0200

    Create a MpdDataAccess.Song module

--- src/MpdDataAccess.ml
+++ src/MpdDataAccess.ml
@@ -1,5 +1,32 @@
 open Lwt.Infix
 
+module Song = struct
+  (** module dedicated to use encapsulate the Mpd.Song type. *)
+  type t = {
+    title : string;
+    artist : string;
+    album : string;
+    time : float;
+    track : int;
+  }
+
+  let from_mpd_song (s : Mpd.Song.t) : t=
+    {
+      title = s.title;
+      artist = s.artist;
+      album = s.album;
+      time = s.time;
+      track = s.track;
+    }
+    (*
+     let title = Mpd.Song.title song in
+  let artist = Mpd.Song.artist song in
+  let album = Mpd.Song.album song in
+  let time = Mpd.Song.duration song in
+  let track = Mpd.Song.track song in
+ *)
+end
+
 (* Initialize random number generator *)
 let () = Random.self_init ()
 
@@ -14,6 +41,23 @@ let build_random_stub prefix =
   if false then Lwt.return_error "nope" (* trick for the stub to return a Result Lwt.t. *)
   else Lwt.return_ok (build_stub 0 [])
 
+let build_random_song title_n artist_n album_n time_n track () =
+    {
+      title = "title" ^ (string_of_int title_n);
+      artist = "artist" ^ (string_of_int artist_n);
+      album = "album" ^ (string_of_int album_n);
+      time = 2.0 +. time_n;
+      track = track;
+    }
+
+(*
+  TODO
+  let build_random_song_list () =
+  let artist_n = Random.int 5 in
+  let title_n = Random.int 10 in
+  let album_n = Random.int 3 in
+  let time_n () = (Random.float 50.0) /. 100. in
+  let rec build i *)
 (** Used to get the internal status *)
 let fetch_status client =
   Mpd.Client_lwt.status client
