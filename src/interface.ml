e8f00661e3d2192d5a3f70831bfdc5f8d5351b13
Cédric Le Moigne <cedlemo@gmx.com>
2018-12-28 19:40:18 +0100

    Improve variables names and comment unused shortcuts

--- src/interface.ml
+++ src/interface.ml
@@ -22,7 +22,7 @@ let result_status_playlist_length = function
                                            | Ok p -> List.length p
                                            | _ -> 0
 
-let rec loop term (e, t) dim client idata =
+let rec loop term (ev_term, ev_mpd) dim client idata =
   let render_and_loop term events idata dim client =
     render idata dim
     >>= fun img ->
@@ -35,9 +35,9 @@ let rec loop term (e, t) dim client idata =
   in
   let wrap_command command =
     match idata with
-    | Error _ -> loop term (event term, t) dim client idata
+    | Error _ -> loop term (event term, ev_mpd) dim client idata
     | Ok d -> (
-      Lwt.cancel t;
+      Lwt.cancel ev_mpd;
       command client d
       >>= fun () ->
         loop term (new_events ()) dim client idata
@@ -45,9 +45,9 @@ let rec loop term (e, t) dim client idata =
   in
   let switch_view view =
     match idata with
-    | Error _ -> loop term (event term, t) dim client idata
+    | Error _ -> loop term (event term, ev_mpd) dim client idata
     | Ok d ->
-        Lwt.cancel t;
+        Lwt.cancel ev_mpd;
         Mpd.Client_lwt.noidle client
         >>= fun _ ->
           Internal_data.create ~view client
@@ -56,9 +56,9 @@ let rec loop term (e, t) dim client idata =
   in
   let move_selection keep_in_bounds =
     match idata with
-    | Error _ -> loop term (event term, t) dim client idata
+    | Error _ -> loop term (event term, ev_mpd) dim client idata
     | Ok data ->
-        Lwt.cancel t;
+        Lwt.cancel ev_mpd;
         let selected = get_selected data in
         let pl_len = get_n_elements data in
         let sel = keep_in_bounds selected pl_len in
@@ -67,18 +67,18 @@ let rec loop term (e, t) dim client idata =
         >>= fun idata' ->
           render_and_loop term (new_events ()) idata' dim client
   in
-  (e <?> t) >>= function
+  (ev_term <?> ev_mpd) >>= function
   | `Mpd_event event_name -> begin
     begin match idata with
           | Error _ -> Internal_data.create client
           | Ok d -> Internal_data.force_update d client
     end
     >>= fun idata' ->
-      let events = (e, listen_mpd_event client) in
+      let events = (ev_term, listen_mpd_event client) in
       render_and_loop term events idata' dim client
   end
   | `Resize dim -> begin
-    Lwt.cancel t;
+    Lwt.cancel ev_mpd;
     Internal_data.update idata client
       >>= fun idata' ->
         render_and_loop term (new_events ()) idata' dim client
@@ -88,23 +88,23 @@ let rec loop term (e, t) dim client idata =
   | `Key (`ASCII 'k', []) ->
     move_selection (fun s l -> if s - 1 < 0 then l - 1 else s - 1)
 (*  | `Key (`Enter, [])     -> wrap_command Commands.rameau_play
-  | `Key (`ASCII 's', []) -> wrap_command Commands.rameau_stop *)
+  | `Key (`ASCII 's', []) -> wrap_command Commands.rameau_stop
   | `Key (`ASCII 'p', []) -> wrap_command Commands.rameau_toggle_pause
   | `Key (`ASCII '+', []) -> wrap_command Commands.rameau_inc_vol
-  | `Key (`ASCII '-', []) -> wrap_command Commands.rameau_decr_vol
+  | `Key (`ASCII '-', []) -> wrap_command Commands.rameau_decr_vol *)
   | `Key (`ASCII '0', []) -> switch_view Internal_data.Help_view
   | `Key (`ASCII '1', []) -> switch_view Internal_data.Queue_view
   | `Key (`ASCII '2', []) -> switch_view Internal_data.Music_db_view
   | `End | `Key (`Escape, []) | `Key (`ASCII 'C', [`Ctrl])
   | `Key (`ASCII 'q', []) -> Commands.rameau_quit client
   | other_keys -> match idata with
-    | Error _ -> loop term (event term, t) dim client idata
+    | Error _ -> loop term (event term, ev_mpd) dim client idata
     | Ok idata' ->
     let shortcuts = get_shortcuts idata' in
-    shortcuts other_keys  client t idata'
+    shortcuts other_keys  client ev_mpd idata'
     >>= function
     | true ->  loop term (new_events ()) dim client idata
-    | false -> render_and_loop term (event term, t) idata dim client
+    | false -> render_and_loop term (event term, ev_mpd) idata dim client
 
 let create client =
   let term = Terminal.create () in
