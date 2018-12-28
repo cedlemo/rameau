704491e3c001276aaccce43971c743a9fb5c1aff
Cédric Le Moigne <cedlemo@gmx.com>
2018-12-28 19:57:44 +0100

    Split Internal_data to View and View_manager in order to avoid cyclic module dependency

--- src/view.ml
+++ src/view.ml
@@ -16,12 +16,14 @@
  * along with Rameau.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
+open Mpd
+open Lwt.Infix
 open Notty
 open Notty_lwt
 module MDA = Mpd_data_access
+module MOS = MDA.Mpd_or_stubs
 
-type view = Queue_view | Help_view | Music_db_view
-(** Used to represent the type of interface to draw *)
+type desc = Queue_view | Help_view | Music_db_view
 
 type status =
   { timestamp: float;        (** Used to limit the number of request to Mpd ie: one each sec *)
@@ -32,7 +34,6 @@ type status =
 
 type panel = { items: string list; selected: int}
 type music_db_selector = { artist: panel; album: panel; song: panel }
-
 type t =
   | Queue of { status: status;
                plist: (MDA.Song.t list, string) result;
@@ -57,24 +58,50 @@ shortcuts = [ `End
             | `Paste of Notty.Unescape.paste
             | `Resize of int * int ] Lwt.t -> t -> bool Lwt.t
 
-val get_status: t -> status
-(** Get the status data from the internal data. *)
-val get_shortcuts: t -> shortcuts
-(** Get the shortcuts handler from the internal data *)
-val get_selected: t -> int
-(** Get the index of the current selected item *)
-val get_n_elements: t -> int
-(** Get the number of items in the current view *)
-val set_selected: (int * int * int) -> t -> t
-(** Set the index of the current selected item *)
-val create:
-  ?view:view -> Mpd.Client_lwt.t -> (t, string) Result.result Lwt.t
-(** Create / fill internal data. *)
-val force_update:
-t -> Mpd.Client_lwt.t -> (t, string) Result.result Lwt.t
-(** Force to update an internal data. *)
-val update:
-(t, string) result -> Mpd.Client_lwt.t -> (t, string) Result.result Lwt.t
-(** Update internal data but with checks for elapsed time in order to limit
- *  the number of request send to Mpd. (not every times a key is pressed. *)
+let view_name = function
+  | Queue _ -> "Queue"
+  | Music_db _ -> "Music Database"
+  | Help _ -> "Help"
+
+let get_status = function
+  | Help {status} -> status
+  | Queue {status; plist; selected} -> status
+  | Music_db {status; db} -> status
+
+let get_selected = function
+  | Help {status} -> -1
+  | Queue {status; plist; selected} -> selected
+  | Music_db {status; db} -> db.artist.selected
+
+let get_shortcuts = function
+  | Help {status; shortcuts} -> shortcuts
+  | Queue {status; plist; selected; shortcuts} -> shortcuts
+  | Music_db {status; db; shortcuts} -> shortcuts
+
+let get_n_elements = function
+  | Help {status} -> -1
+  | Music_db {status; db} ->
+    let n_artist = List.length db.artist.items in
+    let n_album = List.length db.album.items in
+    let n_song = List.length db.song.items in
+    max n_artist n_album
+    |> max n_song
+  | Queue {status; plist; selected} -> match plist with
+    | Error _ -> -1
+    | Ok p -> List.length p
+
+let set_selected (i,i',i'') = function
+  | Help help -> Help help
+  | Queue {status; plist; selected; shortcuts} ->
+    Queue {status; plist; selected = i; shortcuts}
+  | Music_db {status; db; shortcuts} ->
+    let artist_pan = {db.artist with selected = i} in
+    let album_pan = {db.album with selected = i'} in
+    let song_pan = {db.song with selected = i''} in
+    Music_db {status;
+              db = {artist = artist_pan;
+                    album = album_pan;
+                    song = song_pan};
+             shortcuts}
+
 
--- src/view_manager.ml
+++ src/view_manager.ml
@@ -20,139 +20,9 @@ open Mpd
 open Lwt.Infix
 open Notty
 open Notty_lwt
+open View
 module MDA = Mpd_data_access
 module MOS = MDA.Mpd_or_stubs
-
-type view = Queue_view | Help_view | Music_db_view
-
-type status =
-  { timestamp: float;        (** Used to limit the number of request to Mpd ie: one each sec *)
-    state: Mpd.Status.state; (** Mpd state Play, Stop, Pause *)
-    volume: int;             (** Mpd volume *)
-    song: int;               (** The current song. *)
-  }
-
-type panel = { items: string list; selected: int}
-type music_db_selector = { artist: panel; album: panel; song: panel }
-type t =
-  | Queue of { status: status;
-               plist: (MDA.Song.t list, string) result;
-               selected: int;
-               shortcuts: shortcuts }
-  | Music_db of { status: status;
-                  db: music_db_selector ;
-                  shortcuts: shortcuts }
-  | Help of { status: status;
-              shortcuts: shortcuts }
-and
-shortcuts = [ `End
-            | `Key of Notty.Unescape.key
-            | `Mouse of Notty.Unescape.mouse
-            | `Mpd_event of (string, string) result
-            | `Paste of Notty.Unescape.paste
-            | `Resize of int * int ] -> Mpd.Client_lwt.t ->
-            [ `End
-            | `Key of Notty.Unescape.key
-            | `Mouse of Notty.Unescape.mouse
-            | `Mpd_event of (string, string) result
-            | `Paste of Notty.Unescape.paste
-            | `Resize of int * int ] Lwt.t -> t -> bool Lwt.t
-
-let view_name = function
-  | Queue _ -> "Queue"
-  | Music_db _ -> "Music Database"
-  | Help _ -> "Help"
-
-let get_status = function
-  | Help {status} -> status
-  | Queue {status; plist; selected} -> status
-  | Music_db {status; db} -> status
-
-let get_selected = function
-  | Help {status} -> -1
-  | Queue {status; plist; selected} -> selected
-  | Music_db {status; db} -> db.artist.selected
-
-let get_shortcuts = function
-  | Help {status; shortcuts} -> shortcuts
-  | Queue {status; plist; selected; shortcuts} -> shortcuts
-  | Music_db {status; db; shortcuts} -> shortcuts
-
-let get_n_elements = function
-  | Help {status} -> -1
-  | Music_db {status; db} ->
-    let n_artist = List.length db.artist.items in
-    let n_album = List.length db.album.items in
-    let n_song = List.length db.song.items in
-    max n_artist n_album
-    |> max n_song
-  | Queue {status; plist; selected} -> match plist with
-    | Error _ -> -1
-    | Ok p -> List.length p
-
-let set_selected (i,i',i'') = function
-  | Help help -> Help help
-  | Queue {status; plist; selected; shortcuts} ->
-    Queue {status; plist; selected = i; shortcuts}
-  | Music_db {status; db; shortcuts} ->
-    let artist_pan = {db.artist with selected = i} in
-    let album_pan = {db.album with selected = i'} in
-    let song_pan = {db.song with selected = i''} in
-    Music_db {status;
-              db = {artist = artist_pan;
-                    album = album_pan;
-                    song = song_pan};
-             shortcuts}
-
-module CCommands = struct
-let rameau_play client idata =
-  Mpd.Client_lwt.noidle client
-  >>= fun _ ->
-  Mpd.Playback_lwt.play client (get_selected idata)
-  >>= fun _ -> Lwt.return_unit
-
-let rameau_stop client idata =
-  let status = get_status idata in
-  if status.state = Mpd.Status.Play then begin
-    Mpd.Client_lwt.noidle client
-    >>= fun _ ->
-    Mpd.Playback_lwt.stop client
-    >>= fun _ -> Lwt.return_unit
-  end
-  else Lwt.return_unit
-
-let rameau_toggle_pause client idata =
-  Mpd.Client_lwt.noidle client
-  >>= fun _ -> (
-    let status = get_status idata in
-    if status.state = Mpd.Status.Pause then
-      Mpd.Playback_lwt.pause client false
-    else
-      Mpd.Playback_lwt.pause client true
-  )
-  >>= fun _ -> Lwt.return_unit
-
-let rameau_inc_vol client idata =
-  let status = get_status idata in
-  if status.volume < 100 then
-    Mpd.Client_lwt.noidle client
-    >>= fun _ ->
-    Mpd.Playback_options_lwt.setvol client (status.volume + 1)
-    >>= fun _ -> Lwt.return_unit
-  else Lwt.return_unit
-
-let rameau_decr_vol client idata =
-  let status = get_status idata in
-  if status.volume > 0 then
-    Mpd.Client_lwt.noidle client
-    >>= fun _ ->
-    Mpd.Playback_options_lwt.setvol client (status.volume - 1)
-    >>= fun _ -> Lwt.return_unit
-  else Lwt.return_unit
-
-let rameau_quit client =
-  Mpd.Client_lwt.close client
-end
 
 let fetch_music_db client artist_sel album_sel song_sel =
   MOS.fetch_artists_in_music_db ~client ()
@@ -193,19 +63,19 @@ let create ?(view=Queue_view) client =
       let shortcuts events client t idata =
         match events with
         | `Key (`Enter, [])     ->
-          Lwt.cancel t; CCommands.rameau_play client idata
+          Lwt.cancel t; Commands.rameau_play client idata
           >>= fun _ -> Lwt.return true
         | `Key (`ASCII 's', []) ->
-          Lwt.cancel t; CCommands.rameau_stop client idata
+          Lwt.cancel t; Commands.rameau_stop client idata
           >>= fun _ -> Lwt.return true
         | `Key (`ASCII 'p', []) ->
-          Lwt.cancel t; CCommands.rameau_toggle_pause client idata
+          Lwt.cancel t; Commands.rameau_toggle_pause client idata
           >>= fun _ -> Lwt.return true
         | `Key (`ASCII '+', []) ->
-          Lwt.cancel t; CCommands.rameau_inc_vol client idata
+          Lwt.cancel t; Commands.rameau_inc_vol client idata
           >>= fun _ -> Lwt.return true
         | `Key (`ASCII '-', []) ->
-          Lwt.cancel t; CCommands.rameau_decr_vol client idata
+          Lwt.cancel t; Commands.rameau_decr_vol client idata
           >>= fun _ -> Lwt.return true
         | _ -> Lwt.return false
       in
